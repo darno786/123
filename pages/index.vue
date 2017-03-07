d403616443479ee0f9eb401415b5ba1cfb5f7ebc
darno786 <adnan_s14@outlook.com>
2017-03-07 21:04:01 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
     <h1 class="title">
       A project by Darno
     </h1>
+    <h2 class="title">
+      test file
+    </h2>  
     <nuxt-link class="button" to="/about">
       DARNO
     </nuxt-link>
